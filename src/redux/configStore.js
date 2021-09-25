c3074f3880c2968dc80875fcb03809786316ba34
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-09-25 14:10:00 +0700

    Initialize project

---
+++ src/redux/configStore.js
@@ -0,0 +1,7 @@
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+const rootReducer = combineReducers({
+  // state ứng dụng
+});
+
+export const store = createStore(rootReducer, applyMiddleware(thunk));
