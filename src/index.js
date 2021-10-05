6d94209749d95ca42bec3f08144d1ad43c37cfb4
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-10-05 23:07:02 +0700

    Multi-language translation

--- src/index.js
+++ src/index.js
@@ -9,12 +9,31 @@ import "antd/dist/antd.css";
 import reportWebVitals from "./reportWebVitals";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+import { DOMAIN } from "./util/settings/config";
+
+// import đa ngôn ngữ
+import "./i18n";
+//Cấu hình realtime (websocket với signalR)
+import * as signalR from "@aspnet/signalr";
+//Đoạn code để kết nối đến server lắng nghe sự kiện từ server
+export const connection = new signalR.HubConnectionBuilder()
+  .withUrl(`${DOMAIN}/DatVeHub`)
+  .configureLogging(signalR.LogLevel.Information)
+  .build();
+
+connection
+  .start()
+  .then(() => {
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      document.getElementById("root")
+    );
+  })
+  .catch((errors) => {
+    console.log(errors);
+  });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
