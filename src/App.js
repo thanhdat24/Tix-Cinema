e20e5a2bd7b889919c04287a9420fc7d41d05bfb
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-10-08 17:25:11 +0700

    Update Profile

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Contact from "./pages/Contact/Contact";
 import News from "./pages/News/News";
 import Register from "./templates/HomeTemplate/Layout/Register/Register";
 import Detail from "./pages/Detail/Detail";
+import Profile from "./pages/Profile/Profile";
+
 import CheckoutTemplate from "./templates/CheckoutTemplate/CheckoutTemplate";
-import Checkout from "./pages/Checkout/Checkout";
 import React, { Suspense, lazy } from "react";
 import Login from "./pages/Login/Login";
 import Loading from "./components/Loading/Loading";
 import { UserTemplate } from "./templates/UserTemplate/UserTemplate";
+import Checkout from "./pages/BookingTicket/Checkout/Checkout";
 export const history = createBrowserHistory();
 
 const CheckoutTemplateLazy = lazy(() =>
@@ -28,6 +30,8 @@ function App() {
         <HomeTemplate path="/contact" exact Component={Contact} />
         <HomeTemplate path="/news" exact Component={News} />
         <HomeTemplate path="/detail/:id" exact Component={Detail} />
+        <Route path="/profile" exact component={Profile} />
+
         <CheckoutTemplate path="/checkout/:id" exact Component={Checkout} />
         <Route path="/login" exact component={Login} />
         <Route path="/register" exact component={Register} />
