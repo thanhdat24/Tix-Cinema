a6204e4ab2ffe211ab0c13f53c7de6627fc69511
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-10-19 14:56:04 +0700

    Change DOMAIN

--- src/util/settings/config.js
+++ src/util/settings/config.js
@@ -1,8 +1,5 @@
-// import { createMuiTheme } from "@mui/material/styles";
-
-export const DOMAIN = "http://movieapi.cyberlearn.vn";
+export const DOMAIN = "https://movie0706.cybersoft.edu.vn";
 // export const FAKE_AVATAR = `https://i.pravatar.cc/300?u=${avtIdUser}`;
-export const BASE_URL = "https://movie0706.cybersoft.edu.vn/api";
 export const TOKEN = "accessToken ";
 export const GPID = "GP02";
 export const STATUS_CODE = {
@@ -13,14 +10,4 @@ export const STATUS_CODE = {
 
 export const USER_LOGIN = "USER_LOGIN";
 
-// export const theme = createMuiTheme({
-//   breakpoints: {
-//     values: {
-//       xs: 0,
-//       sm: 678,
-//       md: 736,
-//       lg: 768,
-//       xl: 992,
-//     },
-//   },
-// });
+
