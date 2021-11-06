8394704980b8da90f77296631058b9d7a37ed1db
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-11-06 23:40:12 +0700

    Update ThoiLuongDanhGia

--- src/util/settings/config.js
+++ src/util/settings/config.js
@@ -2,6 +2,14 @@ export const DOMAIN = "http://movieapi.cyberlearn.vn";
 // export const FAKE_AVATAR = `https://i.pravatar.cc/300?u=${avtIdUser}`;
 export const TOKEN = "accessToken ";
 export const GPID = "GP02";
+
+export const DATE_BEGIN_DANGCHIEU = "2020-01-01"; // format: yyyy-mm-dd
+export const DATE_END_DANGCHIEU = "2020-12-01";
+
+export const DATE_BEGIN_SAPCHIEU = "2020-12-02";
+
+export const DATE_END_SAPCHIEU = new Date().toISOString()?.slice(0, 10);
+
 export const STATUS_CODE = {
   SUCCESS: 200,
   NOT_FOUND: 404,
---
+++ src/util/settings/useApiThoiLuongDanhGia.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import Axios from "axios";
+export default function UseThoiLuongDanhGia(maPhim) {
+  const [data, setData] = useState({ thoiLuong: "120", danhGia: "10" });
+  const url = `http://movieapi.cyberlearn.vn/api/QuanLyRap/LayThongTinLichChieuPhim?MaPhim=${maPhim}`;
+  useEffect(() => {
+    let getInfoFlimCancel = Axios.CancelToken.source(); // Axios cung cấp, để cancel gọi api khi component bị hủy(bấm chuyển cụm rạp khác)
+    const loadData = async () => {
+      try {
+        // bắt lỗi khi get API, nếu có lỗi thì vào catch
+        const response = await Axios.get(url, {
+          cancelToken: getInfoFlimCancel.token,
+        });
+        setData({
+          thoiLuong:
+            response.data?.heThongRapChieu?.[0]?.cumRapChieu?.[0]
+              ?.lichChieuPhim?.[0]?.thoiLuong, // tách ra thời lượng phim
+          danhGia: response.data.danhGia,
+        });
+      } catch (error) {
+        // vào đây khi có lỗi get api hoặc khi cancel thành công
+        if (Axios.isCancel(error)) {
+          // cancel request thành công
+          // console.log("AxiosCancel: caught cancel");
+        } else {
+          throw error; // báo lỗi get api
+        }
+      }
+    };
+    loadData();
+    return () => {
+      getInfoFlimCancel.cancel(); // unmounting thì cancel request axios
+    };
+  }, []);
+  return { thoiLuong: data.thoiLuong, danhGia: data.danhGia };
+}
