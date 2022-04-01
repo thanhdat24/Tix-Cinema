f8e5541cde385096a775b02139cc93210a76921c
Thành Đạt <54407533+thanhdat24@users.noreply.github.com>
2022-04-01 14:49:24 +0700

    Fix: Footer

--- src/templates/HomeTemplate/Layout/Footer/Footer.js
+++ src/templates/HomeTemplate/Layout/Footer/Footer.js
@@ -1,70 +1,378 @@
-import { AppleOutlined, FacebookOutlined } from "@ant-design/icons";
-import _ from "lodash";
 import React from "react";
-import { useSelector } from "react-redux";
-
-export default function Footer(props) {
-  const { heThongRapChieu } = useSelector(
-    (state) => state.CinemaManagementReducer
-  );
-  // Dùng pick để lấY ra những thuộc tính cân thiết
-  const arrHeThongRap = _.map(heThongRapChieu, (heThongRap) =>
-    _.pick(heThongRap, ["maHeThongRap", "tenHeThongRap", "logo"])
-  );
-  console.log("arr", arrHeThongRap);
+import useStyles from "./style";
+export default function Footer() {
+  const classes = useStyles();
   return (
-    <footer className="py-6 bg-coolGray-100 text-coolGray-900 bg-gray-800">
-      <div className="container px-6 mx-auto space-y-6 divide-y divide-gray-400 md:space-y-12 divide-opacity-50">
-        <div className="grid grid-cols-12">
-          <div className="pb-6 col-span-full md:pb-0 md:col-span-6">
-            <a href="#" className="w-full">
-              <img
-                height="100"
-                width="180"
-                src="./assets/img/logo.png"
-                alt="cyberlearn.vn"
-              />
-            </a>
-          </div>
-          <div className="col-span-6 text-center md:text-left md:col-span-3">
-            <p className="pb-1 text-lg font-medium text-white">PARTNER</p>
-            <div
-              className="grid grid-cols-3 gap-2 w-60"
-              style={{ color: "#fff" }}
-            >
-              {arrHeThongRap.map((htr, index) => {
-                return (
-                  <div key={index}>
-                    <img
-                      src={htr.logo}
-                      style={{ width: 40 }}
-                      alt={htr.maHeThongRap}
-                    />
-                  </div>
-                );
-              })}
+    <div className={classes.footer}>
+      <div className={`${classes.footer__up} row`}>
+        <div className="col-6 col-md-4">
+          <div className={classes.tix__text}>
+            <div className="d-none d-lg-block d-xl-block">
+              <p>TIX</p>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                FAQ
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                Brand Guidelines
+              </a>
             </div>
-          </div>
-          <div className="col-span-6 text-center md:text-left md:col-span-3 text-white">
-            <p className="pb-1 text-lg font-medium">Mobile app</p>
-            <div className="flex text-white">
-              <div className="mr-5">
-                <AppleOutlined className="text-2xl" />
-              </div>
-              <div>
-                <FacebookOutlined className="text-2xl" />
-              </div>
+            <div className="row">
+              <a
+                className="col-6 col-lg-12"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                Thỏa thuận sử dụng
+              </a>
+              <a
+                className="col-6 col-lg-12"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                Chính sách bảo mật
+              </a>
             </div>
           </div>
         </div>
-        <div className="grid justify-center pt-6 lg:justify-between text-white">
-          <div className="flex flex-col self-center text-sm text-center md:block lg:col-start-1 md:space-x-6">
-            <span>©2021 All rights reserved</span>
+        <div className="col-6 col-md-4 footer__company d-none d-lg-block d-xl-block">
+          <p>Đối tác</p>
+          <div>
+            <div className={classes.company__logo}>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/cgv.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/bhd.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/galaxycine.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/cinestar.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/lotte.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+            </div>
+            <div className={classes.company__logo}>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/megags.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/bt.jpg"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/dongdacinema.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/TOUCH.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/cnx.jpg"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+            </div>
+            <div className={classes.company__logo}>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/STARLIGHT.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/dcine.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/zalopay_icon.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/payoo.jpg"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/vcb.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+            </div>
+            <div className={classes.company__logo}>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/AGRIBANK.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/VIETTINBANK.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/IVB.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/123go.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  src="assets/img/logo-connect/laban.png"
+                  alt=""
+                  className={classes.logo}
+                />
+              </a>
+            </div>
           </div>
-
-          <div className=""></div>
+        </div>
+        <div className="col-6 col-md-4 d-flex text-center">
+          <div
+            className={`${classes.social__width} d-none d-lg-block d-xl-block`}
+          >
+            <p className="pb-2">MOBIL APP</p>
+            <div className="footer__mobile d-flex justify-content-around">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  className={classes.logo}
+                  src="assets/img/mobile-system/apple-logo.png"
+                  alt=""
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  className={classes.logo}
+                  src="assets/img/mobile-system/android-logo.png"
+                  alt=""
+                />
+              </a>
+            </div>
+          </div>
+          <div className={classes.social__width}>
+            <p className="d-none d-lg-block d-xl-block pb-2">SOCIAL APP</p>
+            <div className={classes.footer__social}>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  className={classes.logo}
+                  src="assets/img/media/facebook-logo.png"
+                  alt=""
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.cgv.vn/"
+              >
+                <img
+                  className={classes.logo}
+                  src="assets/img/media/zalo-logo.png"
+                  alt=""
+                />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-    </footer>
+      <div className={`${classes.footer__down} container`}>
+        <div className="row">
+          <div className="col-12 col-md-1 p-1 text-center">
+            <img
+              src="assets/img/logo-connect/zion-logo.jpg"
+              alt="company"
+              className={classes.logoCompany}
+            />
+          </div>
+          <div className={`col-12 col-md-9 p-1 text-center text-lg-left`}>
+            <span>TIX - SẢN PHẨM CỦA CÔNG TY CỔ PHẦN ZION</span>
+            <span>
+              Địa chỉ: Z06 Đường số 13, Phường Tân Thuận Đông, Quận 7, Tp. Hồ
+              Chí Minh, Việt Nam.
+            </span>
+            <span>Giấy chứng nhận đăng ký kinh doanh số: 0101659783,</span>
+            <span>
+              đăng ký thay đổi lần thứ 30, ngày 22 tháng 01 năm 2020 do Sở kế
+              hoạch và đầu tư Thành phố Hồ Chí Minh cấp.
+            </span>
+            <span>Số Điện Thoại (Hotline): 1900 545 436</span>
+            <p>
+              Email: <span className="d-inline">support@tix.vn</span>
+            </p>
+          </div>
+          <div
+            className="col-12 col-md-2 p-1 text-center"
+            style={{ position: "static" }}
+          >
+            <img
+              src="assets/img/media/certificate.png"
+              alt="gvm"
+              className={classes.gvm}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
